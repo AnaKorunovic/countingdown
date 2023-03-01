1998e2be4a7ac8206907e6c927d32e863f6506e2
AnaKorunovic <korunovic.ana9@gmail.com>
2023-03-01 17:32:29 +0100

    added pdf logic

---
+++ src/countingdown/logic/pdf-logic.clj
@@ -0,0 +1,25 @@
+(ns logic.pdf-logic
+  (:require [clj-pdf.core :as pdf]
+            ))
+
+(pdf/pdf
+  [{}
+   [:list {:roman true}
+    [:chunk {:style :bold} "a bold item"]
+    "another item"
+    "yet another item"]
+   [:phrase "some text"]
+   [:phrase "some more text"]
+   [:paragraph "yet more text"]]
+  "doc.pdf")
+
+(defn pdf [report]
+  (pdf/*pdf-writer*
+    [{}
+     [:phrase {:style :bold} "A report"]
+     [:paragraph report]]
+    "report.pdf"))
+
+(pdf)
+
+
